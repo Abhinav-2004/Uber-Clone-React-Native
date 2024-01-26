9d6ce7058764b57bee76f247ee7e2e0bbc309e3f
Abhinav Singh <kittusinghranchi@gmail.com>
2024-01-26 17:36:37 +0530

    Top section of HomeScreen is ready

--- uber-clone/App.js
+++ uber-clone/App.js
@@ -2,15 +2,13 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import { store } from './Store';
+import HomeScreen from './screens/HomeScreen';
 //1)setup redux
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-      <Text>Ready to begin??</Text>
-     
-    </View>
+      <HomeScreen/>
     </Provider>
   );
 }
---
+++ uber-clone/screens/HomeScreen.js
@@ -0,0 +1,37 @@
+import {StyleSheet, View, Text, SafeAreaView,Image } from 'react-native';
+import React from 'react';
+import tw from 'tailwind-react-native-classnames';
+
+/*
+
+INCASE YOU WANT MULTIPLE STYLING YOU CAN GIVE LIKE THIS
+
+<Text style={[tw`text-blue-500 p-10`, {backgroundColor:'grey'},
+        styles.text,{borderColor:'black'}
+    ]} >HomeScreen is here</Text>
+*/
+
+const HomeScreen = () => {
+  return (
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <View style={tw`p-5`}>
+        <Image 
+         style={{
+            width:100, height:100, resizeMode:'contain'
+         }}
+         source={{
+            uri:'https://upload.wikimedia.org/wikipedia/commons/thumb/5/58/Uber_logo_2018.svg/2560px-Uber_logo_2018.svg.png'
+         }}
+        />
+      </View>
+    </SafeAreaView>
+  )
+}
+
+export default HomeScreen
+
+const styles=StyleSheet.create({
+    text:{
+        fontWeight:'bold',
+    }
+})
